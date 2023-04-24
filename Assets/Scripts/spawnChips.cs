3fe8ecf4c7e4a700ebaa2b5f38158958c2313bf6
jeronimo_duque <jeronimo.duque@uao.edu.co>
2023-04-24 15:34:09 -0500

    integracion generacion fichas con collider

---
+++ Assets/Scripts/spawnChips.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnChips : MonoBehaviour
+{
+    // Referencia al prefab que queremos instanciar
+    public GameObject objetoPrefab;
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Verificamos si el objeto que colisionó tiene la etiqueta "MiObjeto"
+        if (col.gameObject.CompareTag("MiObjeto"))
+        {
+           // Creamos una nueva instancia del objeto prefab en la posición (0, 0, 0) y sin rotación
+            GameObject objeto = (GameObject) Instantiate(objetoPrefab, new Vector3(1, 1, -2), Quaternion.identity);
+
+            // Podemos hacer cualquier operación que necesitemos con el objeto creado, por ejemplo:
+            objeto.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
