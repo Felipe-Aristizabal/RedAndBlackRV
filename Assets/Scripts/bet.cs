2e339f63e82c7410ef87e269ff70cc1d1002f825
Usuario ILINF <admin-ilinf@uao.edu.co>
2023-04-24 17:20:55 -0500

    Integración de botón para eliminar fichas que están apostadas

---
+++ Assets/Scripts/bet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class bet : MonoBehaviour
+{
+	GameObject[] objectsToDestroy;
+
+	void Update(){
+		objectsToDestroy = GameObject.FindGameObjectsWithTag("Waste");
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+            foreach (GameObject objectToDestroy in objectsToDestroy)
+            {
+                Destroy(objectToDestroy);
+            }
+    }
+}
+
--- Assets/Scripts/betNumber.cs
+++ Assets/Scripts/betNumber.cs
@@ -3,20 +3,22 @@ using UnityEngine.UI;
 
 public class betNumber : MonoBehaviour {
 
-public Text texto;
+    public Text texto;
 
-void OnTriggerEnter(Collider other){
-    if (other.CompareTag("Respawn")){
-        texto.gameObject.SetActive(true);
-        texto.text = "Apostado";
+    void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Respawn")){
+            texto.gameObject.SetActive(true);
+            texto.text = "Apostado";
+            other.gameObject.tag = "Waste";
+        }
     }
-}
 
-void OnTriggerExit(Collider other){
-    if (other.CompareTag("Respawn")){
-        texto.gameObject.SetActive(true);
-        texto.text = "NO Apostado";
+    void OnTriggerExit(Collider other){
+        if (other.CompareTag("Waste")){
+            texto.gameObject.SetActive(true);
+            texto.text = "NO Apostado";
+            other.gameObject.tag = "Respawn";
+        }
     }
-}
 
 }
