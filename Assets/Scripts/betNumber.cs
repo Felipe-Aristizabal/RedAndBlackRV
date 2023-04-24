c3593f68973c6e93e2c3458d3ad34a4a3e6de001
Usuario ILINF <admin-ilinf@uao.edu.co>
2023-04-24 16:13:53 -0500

    Integración inicial del sistema de apuesta

---
+++ Assets/Scripts/betNumber.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class betNumber : MonoBehaviour {
+
+public Text texto;
+
+void OnTriggerEnter(Collider other){
+    if (other.CompareTag("Respawn")){
+        texto.gameObject.SetActive(true);
+        texto.text = "Apostado";
+    }
+}
+
+void OnTriggerExit(Collider other){
+    if (other.CompareTag("Respawn")){
+        texto.gameObject.SetActive(true);
+        texto.text = "NO Apostado";
+    }
+}
+
+}
