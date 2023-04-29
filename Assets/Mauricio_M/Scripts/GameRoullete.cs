78fce09c7b627b9494a3e107ed5ff7315cebd56d
Mauroesco2022 <110541011+Mauroesco2022@users.noreply.github.com>
2023-04-29 11:45:48 -0500

    Logica de rotacion de ruleta

---
+++ Assets/Mauricio_M/Scripts/GameRoullete.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Components;
+using UniRx;
+
+namespace ViewModel
+{
+	
+	public class GameRoullete : ScriptableObject
+	{
+		public GameObject sphere;
+		[Range(0,1000)] public float defaultSpeed = 0;
+		public float currentSpeed;
+
+		public ISubject<int> OnNumber = new Subject<int>();
+		public ISubject<bool> OnRotate = new Subject<bool>();
+	}
+}
