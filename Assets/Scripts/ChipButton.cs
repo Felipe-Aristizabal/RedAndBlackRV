77cae8dc083181382798ea1f3f6f21eba92ce49a
Usuario ILINF <admin-ilinf@uao.edu.co>
2023-04-29 12:01:02 -0500

    implementacion de logica de apuestas

---
+++ Assets/Scripts/ChipButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChipButton : MonoBehaviour
+{
+    public int chipValue;
+    private BetController betController;
+
+    void Start()
+    {
+        betController = GameObject.FindObjectOfType<BetController>();
+        GetComponent<Button>().onClick.AddListener(PlaceBet);
+    }
+
+    void PlaceBet()
+    {
+        betController.PlaceBet(chipValue);
+    }
+}
