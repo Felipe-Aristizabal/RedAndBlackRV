5ae21efcc992fe083c2ee912675733428d7546d5
Usuario ILINF <jeronimo.duque@uao.edu.co>
2023-05-06 09:40:44 -0500

    Logica de apuestas actualizada

--- Assets/Scripts/betNumber.cs
+++ Assets/Scripts/betNumber.cs
@@ -4,16 +4,18 @@ using UnityEngine.UI;
 public class betNumber : MonoBehaviour {
 
     public Text texto;
-    public int totalMoney = 100;
+    private int totalMoney = 25;
     public int currentBet = 0;
 
     public Text totalMoneyText;
     public Text currentBetText;
 
+
     void Start()
 	{
-    totalMoneyText.text =  totalMoney.ToString();
-    currentBetText.text = currentBet.ToString();
+         totalMoneyText.text =  totalMoney.ToString();
+         currentBetText.text = currentBet.ToString();
+    
 	}
 
     void OnTriggerEnter(Collider other){
@@ -21,16 +23,31 @@ public class betNumber : MonoBehaviour {
             texto.gameObject.SetActive(true);
             texto.text = "Apostado";
             other.gameObject.tag = "WhiteBet";
+            currentBet= currentBet+1;
+            totalMoney= totalMoney-1;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
+
         }
         if (other.CompareTag("GreenChip")){
             texto.gameObject.SetActive(true);
             texto.text = "Apostado";
             other.gameObject.tag = "GreenBet";
+            currentBet= currentBet+10;
+            totalMoney= totalMoney-10;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
+
         }
         if (other.CompareTag("RedChip")){
             texto.gameObject.SetActive(true);
             texto.text = "Apostado";
             other.gameObject.tag = "RedBet";
+            currentBet= currentBet+5;
+            totalMoney= totalMoney-5;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
+
         }
     }
 
