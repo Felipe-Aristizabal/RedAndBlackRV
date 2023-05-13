9c3df55c431f2a009e520142b533cb4659f82566
Usuario ILINF <andres.aristizabal_m@uao.edu.co>
2023-05-13 09:58:49 -0500

    Mejora en la actualización del texto

--- Assets/Scripts/betNumber.cs
+++ Assets/Scripts/betNumber.cs
@@ -4,50 +4,58 @@ using UnityEngine.UI;
 public class betNumber : MonoBehaviour {
 
     public Text texto;
-    private int totalMoney = 25;
-    public int currentBet = 0;
 
     public Text totalMoneyText;
     public Text currentBetText;
 
+    private int totalMoney;
+    public int currentBet = 0;
 
     void Start()
 	{
-         totalMoneyText.text =  totalMoney.ToString();
-         currentBetText.text = currentBet.ToString();
-    
+        string moneyText = totalMoneyText.text;
+        int.TryParse(moneyText, out totalMoney);
+        currentBetText.text = currentBet.ToString();
 	}
 
     void OnTriggerEnter(Collider other){
+
+        Vector3 objectPosition = transform.position;
+
         if (other.CompareTag("WhiteChip")){
             texto.gameObject.SetActive(true);
             texto.text = "Apostado";
             other.gameObject.tag = "WhiteBet";
-            currentBet= currentBet+1;
-            totalMoney= totalMoney-1;
+            currentBet = currentBet + 1;
+            totalMoney = totalMoney - 1;
             totalMoneyText.text = totalMoney.ToString();
             currentBetText.text = currentBet.ToString();
-
+            
+            other.transform.position = objectPosition;
         }
-        if (other.CompareTag("GreenChip")){
-            texto.gameObject.SetActive(true);
-            texto.text = "Apostado";
-            other.gameObject.tag = "GreenBet";
-            currentBet= currentBet+10;
-            totalMoney= totalMoney-10;
-            totalMoneyText.text = totalMoney.ToString();
-            currentBetText.text = currentBet.ToString();
 
-        }
         if (other.CompareTag("RedChip")){
             texto.gameObject.SetActive(true);
             texto.text = "Apostado";
             other.gameObject.tag = "RedBet";
-            currentBet= currentBet+5;
-            totalMoney= totalMoney-5;
+            currentBet = currentBet + 5;
+            totalMoney = totalMoney - 5;
             totalMoneyText.text = totalMoney.ToString();
             currentBetText.text = currentBet.ToString();
 
+            other.transform.position = objectPosition;
+        }
+
+        if (other.CompareTag("GreenChip")){
+            texto.gameObject.SetActive(true);
+            texto.text = "Apostado";
+            other.gameObject.tag = "GreenBet";
+            currentBet = currentBet + 10;
+            totalMoney = totalMoney - 10;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
+
+            other.transform.position = objectPosition;
         }
     }
 
@@ -55,36 +63,31 @@ public class betNumber : MonoBehaviour {
         if (other.CompareTag("WhiteBet")){
             texto.gameObject.SetActive(true);
             texto.text = "NO Apostado";
+            currentBet = currentBet - 1;
+            totalMoney = totalMoney + 1;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
             other.gameObject.tag = "WhiteChip";
         }
 
-         if (other.CompareTag("GreenBet")){
+        if (other.CompareTag("RedBet")){
             texto.gameObject.SetActive(true);
             texto.text = "NO Apostado";
-            other.gameObject.tag = "GreenChip";
-        }
-
-         if (other.CompareTag("RedBet")){
-            texto.gameObject.SetActive(true);
-            texto.text = "NO Apostado";
+            currentBet = currentBet - 5;
+            totalMoney = totalMoney + 5;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
             other.gameObject.tag = "RedChip";
         }
+
+        if (other.CompareTag("GreenBet")){
+            texto.gameObject.SetActive(true);
+            texto.text = "NO Apostado";
+            currentBet = currentBet - 10;
+            totalMoney = totalMoney + 10;
+            totalMoneyText.text = totalMoney.ToString();
+            currentBetText.text = currentBet.ToString();
+            other.gameObject.tag = "GreenChip";
+        }    
     }
-
-	
-	public void PlaceBet(int betAmount)
-	{
-		if (totalMoney >= betAmount)
-		{
-			totalMoney -= betAmount;
-			currentBet += betAmount;
-			UpdateUI();
-			}
-	}
-
-	private void UpdateUI()
-	{
-		totalMoneyText.text = totalMoney.ToString();
-		currentBetText.text =  currentBet.ToString();
-	}
 }
