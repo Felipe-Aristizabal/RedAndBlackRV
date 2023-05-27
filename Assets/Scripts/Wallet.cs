a02d7c0ff15bfb29fc28dc151071fc47779c428a
jeronimo-duque <andres.aristizabal_m@uao.edu.co>
2023-05-27 15:23:01 -0500

    Implementación de apuesta para los tercios

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -40,7 +40,7 @@ public class Wallet : MonoBehaviour {
 		lastBet.text = validateNumberBet(lastReportedSensor).ToString();  
 		
 
-		if (isBet == true && rouletteStop == true && lastReportedSensor != "" && spotBet != "" && currentBet != 0 && prueba == true){
+		if (isBet == true && rouletteStop == true && lastReportedSensor != "" && spotBet != "" && currentBet != 0 ){
 			Debug.Log("Lugar mesa"+spotBet);
 			Debug.Log("Reportado: "+ lastReportedSensor);
 			Debug.Log("Apuesta: " + currentBet);
@@ -59,7 +59,8 @@ public class Wallet : MonoBehaviour {
 				priceBet = priceBet * 2;
 				int winBet = totalMoney + priceBet;
 				totalMoneyText.text = winBet.ToString();
-			} if (numberBetRoullete == numberBetTable) {
+			} 
+			if (numberBetRoullete == numberBetTable) {
 				if(numberBetRoullete == 0 || numberBetRoullete == 37){
 					Debug.Log("HAS GANADO APUESTA NÚMERO 0 o 00");
 					int priceBet00 = currentBet;
@@ -77,8 +78,40 @@ public class Wallet : MonoBehaviour {
 					int winBetColor = totalMoney + priceBetColor;
 					totalMoneyText.text = winBetColor.ToString();
 				}
-			} 
-			else {
+			}
+			if (numberBetTable == 38) {
+				if(numberBetRoullete == 1 || numberBetRoullete == 2 || numberBetRoullete == 3 || numberBetRoullete == 4 || numberBetRoullete == 5 || numberBetRoullete == 6 || numberBetRoullete == 7 || numberBetRoullete == 8 || numberBetRoullete == 9 || numberBetRoullete == 10 || numberBetRoullete == 11 || numberBetRoullete == 12) {
+					Debug.Log("HAS GANADO APUESTA NÚMERO THIRD 1");
+					int priceBetThird1 = currentBet;
+					currentBet = 0;
+					currentBetText.text = currentBet.ToString();
+					priceBetThird1 = priceBetThird1 * 3;
+					int winBetThird1 = totalMoney + priceBetThird1;
+					totalMoneyText.text = winBetThird1.ToString();
+				} 
+			}
+			if (numberBetTable == 39) {
+				if(numberBetRoullete == 13 || numberBetRoullete == 14 || numberBetRoullete == 15 || numberBetRoullete == 16 || numberBetRoullete == 17 || numberBetRoullete == 18 || numberBetRoullete == 19 || numberBetRoullete == 20 || numberBetRoullete == 21 || numberBetRoullete == 22 || numberBetRoullete == 23 || numberBetRoullete == 24) {
+					Debug.Log("HAS GANADO APUESTA NÚMERO THIRD 2");
+					int priceBetThird2 = currentBet;
+					currentBet = 0;
+					currentBetText.text = currentBet.ToString();
+					priceBetThird2 = priceBetThird2 * 3;
+					int winBetThird2 = totalMoney + priceBetThird2;
+					totalMoneyText.text = winBetThird2.ToString();
+				} 
+			}
+			if (numberBetTable == 40) {
+				if(numberBetRoullete == 25 || numberBetRoullete == 26 || numberBetRoullete == 27 || numberBetRoullete == 28 || numberBetRoullete == 29 || numberBetRoullete == 30 || numberBetRoullete == 31 || numberBetRoullete == 32 || numberBetRoullete == 33 || numberBetRoullete == 34 || numberBetRoullete == 35 || numberBetRoullete == 36) {
+					Debug.Log("HAS GANADO APUESTA NÚMERO THIRD 3");
+					int priceBetThird3 = currentBet;
+					currentBet = 0;
+					currentBetText.text = currentBet.ToString();
+					priceBetThird3 = priceBetThird3 * 3;
+					int winBetThird3 = totalMoney + priceBetThird3;
+					totalMoneyText.text = winBetThird3.ToString();
+				} 
+			} else {
 				Debug.Log("NADA DE NADA");
 				currentBet = 0;
 				currentBetText.text = currentBet.ToString();
@@ -100,28 +133,13 @@ public class Wallet : MonoBehaviour {
 		currentBetText.text = currentBet.ToString();
 	}
 
-	public void apostar(){
-		Debug.Log("Presione el boton apostar");
-		
-		// roulleteRun = this.refRotateAround.stop;
-		// while(roulleteRun == false){
-		// 	Debug.Log("Entré al while");
-			
-		// 	roulleteRun = this.refRotateAround.stop;
-		// 	if(roulleteRun){
-		// 		Debug.Log("salio del ciclo");
-		// 		break;
-		// 	}
-		// };
-	 } 
-
 	public int validateNumberBet(string lastReportedSensor){
 		switch (lastReportedSensor){
 			case "Bet Red":
-			return 40;
+			return 41;
 			
 			case "Bet Black":
-			return 41;
+			return 42;
 			
 			case "Bet 1": 
 			return 1;
@@ -236,6 +254,15 @@ public class Wallet : MonoBehaviour {
 			
 			case "Bet 0": 
 			return 0;
+
+			case "Bet Third 1":
+			return 38;
+
+			case "Bet Third 2":
+			return 39;
+
+			case "Bet Third 3":
+			return 40;
 
 			default:
 			return -1;
