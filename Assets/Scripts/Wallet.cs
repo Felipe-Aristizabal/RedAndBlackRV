71a47c0af45536ec61dd67f582004159992d91be
Usuario ILINF <andres.aristizabal_m@uao.edu.co>
2023-05-29 16:03:08 -0500

    BUILD 001

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -27,6 +27,9 @@ public class Wallet : MonoBehaviour {
 
 	public string sceneName = "finalScene";
 
+	public AudioSource winSound;
+	public AudioSource loseSound;
+
 	void Start () {
 	    string moneyText = totalMoneyText.text;
         int.TryParse(moneyText, out totalMoney);
@@ -51,75 +54,51 @@ public class Wallet : MonoBehaviour {
 
 			if (colorBetRoullete == colorBetTable){
 				Debug.Log("HAS GANADO APUESTA COLOR");
-				int priceBet = currentBet;
-				currentBet = 0;
-				currentBetText.text = currentBet.ToString();
-				priceBet = priceBet * 2;
-				int winBet = totalMoney + priceBet;
-				totalMoneyText.text = winBet.ToString();
+				apostar(2);
 				isBet = false;
+				winSound.Play();
 			} 
 			if (numberBetRoullete == numberBetTable) {
 				if(numberBetRoullete == 0 || numberBetRoullete == 37){
 					Debug.Log("HAS GANADO APUESTA NÚMERO 0 o 00");
-					int priceBet00 = currentBet;
-					currentBet = 0;
-					currentBetText.text = currentBet.ToString();
-					priceBet00 = priceBet00 * 44;
-					int winBet00 = totalMoney + priceBet00;
-					totalMoneyText.text = winBet00.ToString();
+					apostar(45);	
 					isBet = false;
+					winSound.Play();
 				} else {
 					Debug.Log("HAS GANADO APUESTA NÚMERO");
-					int priceBetColor = currentBet;
-					currentBet = 0;
-					currentBetText.text = currentBet.ToString();
-					priceBetColor = priceBetColor * 18;
-					int winBetColor = totalMoney + priceBetColor;
-					totalMoneyText.text = winBetColor.ToString();
+					apostar(18);
 					isBet = false;
+					winSound.Play();
 				}
 			}
 			if (numberBetTable == 38) {
 				if(numberBetRoullete == 1 || numberBetRoullete == 2 || numberBetRoullete == 3 || numberBetRoullete == 4 || numberBetRoullete == 5 || numberBetRoullete == 6 || numberBetRoullete == 7 || numberBetRoullete == 8 || numberBetRoullete == 9 || numberBetRoullete == 10 || numberBetRoullete == 11 || numberBetRoullete == 12) {
 					Debug.Log("HAS GANADO APUESTA NÚMERO THIRD 1");
-					int priceBetThird1 = currentBet;
-					currentBet = 0;
-					currentBetText.text = currentBet.ToString();
-					priceBetThird1 = priceBetThird1 * 3;
-					int winBetThird1 = totalMoney + priceBetThird1;
-					totalMoneyText.text = winBetThird1.ToString();
+					apostar(3);
 					isBet = false;
+					winSound.Play();
 				} 
 			}
 			if (numberBetTable == 39) {
 				if(numberBetRoullete == 13 || numberBetRoullete == 14 || numberBetRoullete == 15 || numberBetRoullete == 16 || numberBetRoullete == 17 || numberBetRoullete == 18 || numberBetRoullete == 19 || numberBetRoullete == 20 || numberBetRoullete == 21 || numberBetRoullete == 22 || numberBetRoullete == 23 || numberBetRoullete == 24) {
 					Debug.Log("HAS GANADO APUESTA NÚMERO THIRD 2");
-					int priceBetThird2 = currentBet;
-					currentBet = 0;
-					currentBetText.text = currentBet.ToString();
-					priceBetThird2 = priceBetThird2 * 3;
-					int winBetThird2 = totalMoney + priceBetThird2;
-					totalMoneyText.text = winBetThird2.ToString();
+					apostar(3);
 					isBet = false;
+					winSound.Play();
 				} 
 			}
 			if (numberBetTable == 40) {
 				if(numberBetRoullete == 25 || numberBetRoullete == 26 || numberBetRoullete == 27 || numberBetRoullete == 28 || numberBetRoullete == 29 || numberBetRoullete == 30 || numberBetRoullete == 31 || numberBetRoullete == 32 || numberBetRoullete == 33 || numberBetRoullete == 34 || numberBetRoullete == 35 || numberBetRoullete == 36) {
 					Debug.Log("HAS GANADO APUESTA NÚMERO THIRD 3");
-					int priceBetThird3 = currentBet;
-					currentBet = 0;
-					currentBetText.text = currentBet.ToString();
-					priceBetThird3 = priceBetThird3 * 3;
-					int winBetThird3 = totalMoney + priceBetThird3;
-					totalMoneyText.text = winBetThird3.ToString();
+					apostar(3);
 					isBet = false;
+					winSound.Play();
 				} 
 			} else {
 				Debug.Log("NADA DE NADA");
-				currentBet = 0;
-				currentBetText.text = currentBet.ToString();
+				apostar(0);
 				isBet = false;
+				loseSound.Play();
 			}
 		}
 
@@ -128,6 +107,14 @@ public class Wallet : MonoBehaviour {
 		}
     }
 	
+	public void apostar(int number) {
+		totalMoney = totalMoney + currentBet * number;
+		currentBet = 0;
+		currentBetText.text = currentBet.ToString();
+		totalMoneyText.text = totalMoney.ToString();
+	}
+
+
 	public void apostarEnPantalla(int ficha) {
 		currentBet = currentBet + ficha;
 		totalMoney = totalMoney - ficha;
